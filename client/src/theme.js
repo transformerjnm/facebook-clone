98dcb429482690c03228a51afbf8b27c6516e913
Chad Altenberger <chadaltenberger@gmail.com>
2021-01-07 06:53:50 -0500

    Removed overrides from theme.js and added a custom css rule to remove the hover effect from the NavigationIconLink buttons #6

--- client/src/theme.js
+++ client/src/theme.js
@@ -2,15 +2,6 @@ import { createMuiTheme } from '@material-ui/core/styles';
 
 /** Light Theme */
 export const lightTheme = createMuiTheme({
-  overrides: {
-    MuiIconButton: {
-      root: {
-        '&:hover': {
-          backgroundColor: '$labelcolor'
-        }
-      }
-    }
-  },
   palette: {
     primary: {
       light: '#ffffff',
