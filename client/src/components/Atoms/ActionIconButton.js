40a6fd6df07ca523064f61e26b71ef2b79c744dd
blaine young <blaine.young3553@gmail.com>
2021-01-09 12:24:20 -0500

    started the creation of the ActionIconButton #11

--- client/src/components/Atoms/ActionIconButton.js
+++ client/src/components/Atoms/ActionIconButton.js
@@ -1,4 +1,14 @@
-import { Button } from '@material-ui/core';  
+import { Fragment } from 'react';
+import { Button, IconButton } from '@material-ui/core'; 
+import PeopleIcon from '@material-ui/icons/People';
+import StorefrontIcon from '@material-ui/icons/Storefront';
+import EventAvailableIcon from '@material-ui/icons/EventAvailable';
+import BookmarkIcon from '@material-ui/icons/Bookmark';
+import FlagIcon from '@material-ui/icons/Flag';
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import StarIcon from '@material-ui/icons/Star';
+
+
 
 /**
  * Summary: 
@@ -11,8 +21,11 @@ import { Button } from '@material-ui/core';
  */
 export default props => {
 	return (
-		<div>
-			<p>ActionButton.js</p>
-		</div>
+		<>
+			<IconButton>
+				<PeopleIcon/>
+				{props.buttonText}
+			</IconButton>
+		</>
 	);
 }
