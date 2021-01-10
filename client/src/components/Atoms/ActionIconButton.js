e7264904e089cecc254c5f6860cf08705e757eff
blaine young <blaine.young3553@gmail.com>
2021-01-09 19:57:28 -0500

    finished the atom and made it more dynamic #11
    by using props.children i was able to remove my switch conditional statement

--- client/src/components/Atoms/ActionIconButton.js
+++ client/src/components/Atoms/ActionIconButton.js
@@ -1,25 +1,13 @@
-import { Fragment } from 'react';
 import { Box, Button, FormControl, IconButton } from '@material-ui/core'; 
-import PeopleIcon from '@material-ui/icons/People';
-import StorefrontIcon from '@material-ui/icons/Storefront';
-import EventAvailableIcon from '@material-ui/icons/EventAvailable';
-import BookmarkIcon from '@material-ui/icons/Bookmark';
-import FlagIcon from '@material-ui/icons/Flag';
-import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
-import StarIcon from '@material-ui/icons/Star';
-import { spacing } from '@material-ui/system';
-
-
 /**
  * Summary: 
  * Button that has text and a icon. When clicked it fires an action.
  * 
  * Props: 
- * buttonIcon
- * buttonAction
- * buttonText
+ * buttonAction: a callback function that is fired when the ActionIconButton is fired
+ * buttonText: text that is shown next to the icon
  */
-export default ({buttonIcon, buttonAction, buttonText}) => {
+export default props => {
 	let actionIconButton;
 	const iconButtonStyles = {
 		color: 'black'
@@ -28,34 +16,18 @@ export default ({buttonIcon, buttonAction, buttonText}) => {
 		display: 'flex',
 		justifyContent: 'center'
 	};
-	const iconStyles = {
-		marginRight: 'auto'
-	};
-
-	switch(buttonIcon){
-		case "people": actionIconButton = <Button style={iconButtonStyles}>
-											<Box mr={1.5} style={boxStyles}>
-												<PeopleIcon color="secondary" fontSize="large" />
-											</Box>
-											<Box mr="auto" style={boxStyles}>
-												{buttonText}
-											</Box>
-										</Button>;
-		break;
-		case "store": actionIconButton = <Button style={iconButtonStyles}>
-											<Box mr={1.5} style={boxStyles}>
-												<StorefrontIcon color="secondary" fontSize="large" />
-											</Box>
-											<Box mr="auto" style={boxStyles}>
-												{buttonText}
-											</Box>
-										</Button>;
-	}
 
 	return (
 		<>
 			<FormControl fullWidth hiddenLabel>
-				{actionIconButton}
+				<Button style={iconButtonStyles}>
+					<Box mr={1.5} style={boxStyles}>
+						{props.children}
+					</Box>
+					<Box mr="auto" style={boxStyles}>
+						{props.buttonText}
+					</Box>
+				</Button>
 			</FormControl>
 		</>
 	);
