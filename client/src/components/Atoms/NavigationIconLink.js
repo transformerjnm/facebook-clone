6da045cea45f41c1a421cb43775cfa9d2343e7d5
Chad Altenberger <chadaltenberger@gmail.com>
2021-01-13 18:28:13 -0500

    Created component for the nav links with styling for active class #12

--- client/src/components/Atoms/NavigationIconLink.js
+++ client/src/components/Atoms/NavigationIconLink.js
@@ -1,17 +1,42 @@
-import { Button } from '@material-ui/core';  
+import { IconButton } from '@material-ui/core';
+
+import { NavLink } from 'react-router-dom';
+import { lightTheme } from '../../theme';
+
+import styles from '../Atoms/Atom-Styles/NavigationIconLink.module.css';
 
 /**
  * Summary:
  * Button Icon with no text that is a navigation link. Also has navigation styles.
- * 
- * Props: 
- * navigationIcon
- * navigationLinkDestination
+ * Will pass the navIcon from MUI and the navLinkDestination as props when rendering
+ * the different icons.
+ *
+ * Props:
+ * navIcon
+ * navLinkDestination
  */
-export default props => {
-	return (
-		<div>
-			<p>Navigation Link</p>
-		</div>
-	);
-}
+
+export default ({ navLinkDestination, navIcon }) => {
+  return (
+    <div className={styles.navIconContainer}>
+      <IconButton
+        style={{ background: 'transparent' }}
+        disableFocusRipple='true'
+        disableRipple='true'
+        children={
+          <NavLink
+            exact
+            to={navLinkDestination}
+            style={{
+              color: lightTheme.palette.primary.dark,
+              paddingTop: '5px'
+            }}
+            activeClassName={styles.active}
+          >
+            {navIcon}
+          </NavLink>
+        }
+      ></IconButton>
+    </div>
+  );
+};
--- client/src/components/Pages/HomePage.js
+++ client/src/components/Pages/HomePage.js
@@ -1,21 +1,33 @@
-import { } from '@material-ui/core';  
+import {} from '@material-ui/core';
 import CreateNewPost from '../Organisms/CreateNewPost';
 import HomePageHeader from '../Organisms/HomePageHeader';
 import UnorderedListOfImagesWithText from '../Organisms/UnorderedListOfImagesWithText';
 import UserPost from '../Organisms/UserPost';
 import ViewAndAddPostComment from '../Organisms/ViewAndAddPostComment';
+import { lightTheme } from '../../theme';
+
+// import '../Atoms/Atom-Styles/NavigationIconLink.css';
+
+//MUI Icons
+// import Button from '@material-ui/core/Button';
+// import HomeIcon from '@material-ui/icons/Home';
+// import TvIcon from '@material-ui/icons/Tv';
+// import StorefrontIcon from '@material-ui/icons/Storefront';
+// import GroupWorkIcon from '@material-ui/icons/GroupWork';
+// import GamepadIcon from '@material-ui/icons/Gamepad';
+// import NavigationIconLink from '../Atoms/NavigationIconLink';
 
 /**
  * Summary:
  * Home page that has three columns.
- * 
- * Props: 
- * 
+ *
+ * Props:
+ *
  */
 export default props => {
-  	return (
-    	<div>
-     		<p>Home Page</p>
-    	</div>
-  	);
-}
+  return (
+    <div>
+      <p>Home Page</p>
+    </div>
+  );
+};
