cb4852746ff4df5679604e612ddfabbc3e878aa4
transformerjnm <46799492+transformerjnm@users.noreply.github.com>
2021-01-13 12:05:20 -0500

    Created the post interaction cluster #19
    Created the post interaction cluster molecule. It contains the buttons to interact and  displays the number of interactions the post has.

--- client/src/components/Atoms/ActionIconButton.js
+++ client/src/components/Atoms/ActionIconButton.js
@@ -1,4 +1,5 @@
-import { Box, Button, FormControl, IconButton } from '@material-ui/core'; 
+import { Box, Button, FormControl, Typography } from '@material-ui/core'; 
+import { lightTheme } from '../../theme';
 /**
  * Summary: 
  * Button that has text and a icon. When clicked it fires an action.
@@ -11,18 +12,20 @@ import { Box, Button, FormControl, IconButton } from '@material-ui/core';
 export default props => {
 	const boxStyles = {
 		display: 'flex',
-		justifyContent: 'center'
+		justifyContent: 'center',
 	};
-
+	const centerButtonStyle = {
+		margin: 'auto'
+	}
 	return (
 		<>
 			<FormControl fullWidth hiddenLabel>
-				<Button onClick={() => props.buttonAction()}>
+				<Button style={centerButtonStyle} onClick={() => props.buttonAction()}>
 					<Box mr={1.5} style={boxStyles}>
 						{props.children}
 					</Box>
 					<Box mr="auto" style={boxStyles}>
-						{props.buttonText}
+						<Typography style={{color: lightTheme.palette.text.secondary}} variant="body1">{props.buttonText}</Typography>
 					</Box>
 				</Button>
 			</FormControl>
--- client/src/components/Molecules/PostInteractionCluster.js
+++ client/src/components/Molecules/PostInteractionCluster.js
@@ -1,17 +1,73 @@
-import { } from '@material-ui/core';  
+import { Typography, Grid, Divider } from '@material-ui/core';  
 import ActionIconButton from '../Atoms/ActionIconButton';
-
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
+import ReplyIcon from '@material-ui/icons/Reply';
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+import { lightTheme } from '../../theme';
 /**
  * Summary:
- * Group of button that appear on the bottom of all post. Shows how many and allows you to like, comments, and share.
+ * Group of buttons that appear on the bottom of all post. Shows how many interaction you have and allows you to like, comments, and share on post.
  * 
  * Props: 
- * 
+ * addLikeToPost: function to increase number of likes
+ * openCommentsForPost: function to open comment section
+ * sharePost: function to allow user to share the post
+ * numberOfPostLikes: integer of number of likes
+ * numberOfPostComments: integer of number of comments
+ * numberOfPostShares: integer of number of post
  */
 export default props => {
+	const secondaryTextStyle = {
+		color: lightTheme.palette.text.secondary,
+		display: 'inline',
+	};
+	/**
+	 * If their post has any interactions for like, comments, or shares show the number of interactions they have. 
+	 * Also contains the light grey line divider that divides the interaction buttons and the number of interaction section.
+	 */
+	let showNumberOfInteractions = () => {
+		if(props.numberOfPostShares > 0 || props.numberOfPostLikes > 0 || props.numberOfPostComments > 0) {
+			return (
+				<>
+					<Grid item xs="6" style={{padding: '.5rem 0'}}>
+						<Grid container justify="flex-start">
+							<Grid item>
+								<ThumbUpIcon style={secondaryTextStyle} fontSize="small" />
+								<Typography style={secondaryTextStyle} variant="subtitle1">
+									{props.numberOfPostLikes ? props.numberOfPostLikes : 0}
+								</Typography>
+							</Grid>
+						</Grid>
+					</Grid>
+					<Grid item xs="6" style={{padding: '.5rem 0'}}>
+						<Grid container justify="flex-end">
+							<Grid item>
+								<Typography style={secondaryTextStyle} variant="subtitle1">
+									{props.numberOfPostComments ? props.numberOfPostComments : 0} Comments {props.numberOfPostShares ? props.numberOfPostShares : 0} Shares
+								</Typography>
+							</Grid>
+						</Grid>
+					</Grid>
+					<Grid item xs="12">
+						<Divider />
+					</Grid>	
+				</>
+			);
+		}
+	};
 	return (
-		<div>
-      		<p>Post Interaction Cluster</p>
-    	</div>
+		<Grid container alignContent="center">
+			{showNumberOfInteractions()}
+			<Grid item xs="4">
+				<ActionIconButton buttonText="Like" buttonAction={props.addLikeToPost}><ThumbUpAltIcon style={secondaryTextStyle} /></ActionIconButton>
+			</Grid>
+			<Grid item xs="4">
+				<ActionIconButton buttonText="Comment" buttonAction={props.openCommentsForPost}><ChatBubbleOutlineIcon style={secondaryTextStyle} /></ActionIconButton>
+			</Grid>
+			<Grid item xs={4}>								
+				<ActionIconButton buttonText="Share" buttonAction={props.sharePost}><ReplyIcon style={secondaryTextStyle} /></ActionIconButton>
+			</Grid>
+    	</Grid>
   	);
-}
+};
