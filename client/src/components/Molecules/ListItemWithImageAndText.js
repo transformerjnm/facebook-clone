cb66bc5d8f7c876b3a3943cdbc7a192f84c48943
Blaine Young <blaine.young3553@gmail.com>
2021-01-13 11:35:25 -0500

    updated component comment and removed zombie code #16

--- client/src/components/Molecules/ListItemWithImageAndText.js
+++ client/src/components/Molecules/ListItemWithImageAndText.js
@@ -1,20 +1,18 @@
-import { List, ListItem, ListItemText, ListItemIcon } from '@material-ui/core';
-import CircleImage from '../Atoms/CircleImage';
+import { ListItem, ListItemText, ListItemIcon } from '@material-ui/core';
 
 /**
  * Summary:
  * List Item that has a image/icon on the left and text on the right beside it. 
  * For use with unordered list and can take a action to be fired onClick. this
- * component must be wrapped with a material ui component "List"
+ * component must be wrapped with a material ui component "List". This component
+ * should wrap either a Icon or CircleImage component that contain the styles of
+ * marginLeft: "auto"
  * 
  * Props: 
  * Text: text that is shown next to an icon or image
  * action: a function that is fired on click of a ListItem
  */
 export default props => {
-	const listItemIconStyles = {
-		marginLeft: 'auto'
-	};
 	
   	return (
     	<>
