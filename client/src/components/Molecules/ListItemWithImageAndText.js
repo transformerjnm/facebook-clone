04b2b67acfa95ab70898520f6e2c197a9163584a
blaine young <blaine.young3553@gmail.com>
2021-01-13 09:58:43 -0500

    updated comment and added on click action #16

--- client/src/components/Molecules/ListItemWithImageAndText.js
+++ client/src/components/Molecules/ListItemWithImageAndText.js
@@ -4,12 +4,12 @@ import CircleImage from '../Atoms/CircleImage';
 /**
  * Summary:
  * List Item that has a image/icon on the left and text on the right beside it. 
- * For use with unordered list and can take a action to be fired onClick.
+ * For use with unordered list and can take a action to be fired onClick. this
+ * component must be wrapped with a material ui component "List"
  * 
  * Props: 
- * Image
- * Text
- * action
+ * Text: text that is shown next to an icon or image
+ * action: a function that is fired on click of a ListItem
  */
 export default props => {
 	const listItemIconStyles = {
@@ -18,7 +18,7 @@ export default props => {
 	
   	return (
     	<>
-			<ListItem button>
+			<ListItem button onClick={() => props.action()}>
 				<ListItemIcon children={props.children} />
 				<ListItemText primary={props.text} style={{marginLeft: '6px'}}/>
 			</ListItem>
