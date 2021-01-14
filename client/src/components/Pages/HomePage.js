35de518fb2903a22e4fb1a397b643b48d126cf8e
transformerjnm <46799492+transformerjnm@users.noreply.github.com>
2021-01-14 14:24:12 -0500

    removed stuff I forgot in the home page #17

--- client/src/components/Pages/HomePage.js
+++ client/src/components/Pages/HomePage.js
@@ -18,26 +18,6 @@ import TextInput from '../Atoms/TextInput';
  */
 export default props => {
   	return (
-    	<div>
-			<CircleImageWithTextInput 
-				inputVariant="comment"
-				imageSrc="https://images.unsplash.com/photo-1610573007685-e56716e3e4e7?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-				imageAlt="Women holding a camera while walking."
-				isOnline={true}
-			/>
-			<CircleImageWithTextInput 
-				inputVariant="comment"
-				imageSrc="https://images.unsplash.com/photo-1610573007685-e56716e3e4e7?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-				imageAlt="Women holding a camera while walking."
-			/>
-			<CircleImageWithTextInput 
-				inputVariant="comment"
-			/>
-			<CircleImageWithTextInput 
-				inputVariant="search"
-				imageSrc="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAASwAAACoCAMAAABt9SM9AAAAY1BMVEU6WJ7///9ofbM1VJwdRpb8/f8aRJUpTJnw8vdsf7PByd4lSpjT2egwUZvX3Os3Vp1LZ6dfda5YcKuZpsnO1OWtt9N7jLv19/uImMHo7PTc4OyxvdYUQZRZcq0AMI7i5/EAOJBh473TAAABh0lEQVR4nO3b0W7aQBBAURIDTuK4DoEEaNqS///KCpWIXbKsmJfaD+d8wejKsryj9WwGAAAAAAAAAAAAAAAAAABQNAxDczb2NFPW9G37sno8U+uapntdbx62i7uz5dgzTVW72qSdxKro1pelxLqmeyu0Equofy+1EqtkaLdi3arfFVuJVdJuxLrZ/NtHg1hXDeVWYhU0r3mj/fM/P9uxJ5ugZpW1+ui/jD3YFDX3Wax+GHugKctjLeZjzzNpF7G81ms8WQFiBYgVINZtlke/fmex/ixPVMssr5xzTtRK1WNtxUrVYx26seeblHqsZwfpVD3Wk1ipeqwPa4dUPdZarFQ91k6sVD2WiyGZeqwXW8BUd/hxdMgaPZ1sZmJluqN5toNffLYnWhXYOgSIFSBWgFgBYgWIFSBWgFgBYgWIFSBWgFgBYgWIFSBWgFgBYgWIFSBWgFgBYgWIFSBWgFgBYgWIFSBWgFgBF/8bilXT3G8fzvYudAMAAAAAAAAAAAAAAAAAAPB//AX5RQ4cWZetrAAAAABJRU5ErkJggg=="
-				imageAlt="Facebook logo"
-			/>
-    	</div>
+    	<p>Hello World. Welcome to the Home Page!</p>
   	);
 }
