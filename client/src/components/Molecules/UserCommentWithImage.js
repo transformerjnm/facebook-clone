59dde7de35c9782c098ac741da69dc1cf3ca62de
blaine young <blaine.young3553@gmail.com>
2021-01-20 16:20:10 -0500

    started to create UserCommentWithImage component #15

--- client/src/components/Molecules/UserCommentWithImage.js
+++ client/src/components/Molecules/UserCommentWithImage.js
@@ -1,20 +1,69 @@
-import { } from '@material-ui/core';  
+import { Grid, Typography } from '@material-ui/core';  
 import CircleImage from '../Atoms/CircleImage';
-
+import { lightTheme } from '../../theme';
 /**
  * Summary:
  * A comment that has the users profile picture, username, and comment.
  * 
  * Props: 
- * Image
+ * children: CircleImage that is wrapped by this component
  * Name
  * AuthorComment
  * 
  */
 export default props => {
+	const commentContainer = {
+		width: 'auto',
+		backgroundColor: 'lightgrey',
+		borderRadius: '15px',
+		padding: '.25rem' 
+	};
 	return (
-		<div>
-			<p>UserCommentWithImage.js</p>
-		</div>
+		<Grid container alignContent="center" spacing={1}>
+			<Grid item>
+				<Grid container justify="flex-start">
+					{props.children}
+				</Grid>
+			</Grid>
+			<Grid item>
+				<div style={commentContainer}>
+					<Grid container>
+						<Grid item xs={12}>
+							<Typography variant="subtitle2">
+								{props.name}
+							</Typography>
+						</Grid>
+						<Grid item>
+							<Typography variant="body2">
+								{props.authorComment}
+							</Typography>
+						</Grid>
+					</Grid>
+				</div>
+				<Grid container>
+					<Grid item xs={1}>
+						<Grid container>
+							<Typography variant="body2">
+								Like
+							</Typography>
+						</Grid>
+					</Grid>
+					<Grid item xs={1}>
+						<Grid container>
+							<Typography variant="body2">
+								Reply
+							</Typography>
+						</Grid>
+					</Grid>
+					<Grid item xs={1}>
+						<Grid container justify="center">
+							<Typography variant="body2">
+								{props.timePosted}
+							</Typography>
+						</Grid>
+					</Grid>
+				</Grid>
+			</Grid>
+		</Grid>
 	);
 }
