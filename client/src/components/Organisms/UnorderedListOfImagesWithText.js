76d25cc384cf6aaa78c281dcc20a6d399b903665
blaine young <blaine.young3553@gmail.com>
2021-01-21 21:27:48 -0500

    started to create the organism #20

--- client/src/components/Organisms/UnorderedListOfImagesWithText.js
+++ client/src/components/Organisms/UnorderedListOfImagesWithText.js
@@ -1,6 +1,7 @@
-import { List, ListItem, ListItemText } from '@material-ui/core';  
+import { useState } from 'react';
+import { List, ListItem, ListItemText, Paper } from '@material-ui/core';  
 import ListItemWithImageAndText from '../Molecules/ListItemWithImageAndText';
-
+import HomeIcon from '@material-ui/icons/Home';
 /**
  * Summary:
  * Displays a unordered list of images with text from an array that you can click on.
@@ -10,13 +11,25 @@ import ListItemWithImageAndText from '../Molecules/ListItemWithImageAndText';
  */
 export default props => {
 	const renderListItemsToScreen = () => {
-		props.listItemsArray.foreach(() => {
-
+		props.listItemsArray.forEach(item => {
+			console.log(item)
+			return(
+				<ListItemWithImageAndText text={item.itemName}>
+					item.imagePath
+				</ListItemWithImageAndText>
+			);
 		});
-	}
+	};
+	const paperStyles = {
+		width: '25vw'
+	};
 	return (
-		<div>
-			<p>unorderd List</p>
-		</div>
+		<>
+			<Paper style={paperStyles}>
+				<List>
+					{renderListItemsToScreen()}
+				</List>
+			</Paper>
+		</>
   	);
 }
