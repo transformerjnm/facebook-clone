a28a7d531c773742ccbe3e5f250344846371b135
blaine young <blaine.young3553@gmail.com>
2021-01-24 23:15:43 -0500

    implemented requested changes #20

--- client/src/components/Organisms/UnorderedListOfImagesWithText.js
+++ client/src/components/Organisms/UnorderedListOfImagesWithText.js
@@ -1,36 +1,36 @@
-import { List, ListItem, ListItemText, Paper } from '@material-ui/core';  
+import { List } from '@material-ui/core';  
 import ListItemWithImageAndText from '../Molecules/ListItemWithImageAndText';
 import { lightTheme } from '../../theme';
+
 /**
  * Summary:
  * Displays a unordered list of images with text from an array that you can click on.
  * 
  * Props: 
  * listItemsArray: array of objects where each object contains the image, title, and action.
+ * variant: a string that states which type of unordered list is rendered: sidebar or contacts
  */
 export default props => {
 	var listItemsArray = [];
+	var keyForEachListItem = 1;
 	const renderListItemsToScreen = () => {
 		props.listItemsArray.forEach(item => {
 			listItemsArray.push(
-				<ListItemWithImageAndText text={item.name} action={item.action}>
+				<ListItemWithImageAndText text={item.name} key={keyForEachListItem}>
 					{item.image}
 				</ListItemWithImageAndText>
 			);
 		});
 	};
-	const paperStyles = {
-		width: '25vw',
+	const containerStyles = {
 		backgroundColor: lightTheme.palette.primary.main
 	};
 	return (
-		<>
-			<Paper style={paperStyles}>
-				<List>
-					{renderListItemsToScreen()}
-					{listItemsArray}
-				</List>
-			</Paper>
-		</>
+		<div style={containerStyles}>
+			<List>
+				{renderListItemsToScreen()}
+				{listItemsArray}
+			</List>
+		</div>
   	);
 }
