00d35d44462080ab46bd4ee5b428a12fbe8e7941
transformerjnm <46799492+transformerjnm@users.noreply.github.com>
2021-01-25 20:18:46 -0500

    Updated comment and removed unused import for UnorderedListOfIamgesWithText. #20
    Removed unused import, updated the comment props section, and passed the action prop to the list item.

--- client/src/components/Organisms/UnorderedListOfImagesWithText.js
+++ client/src/components/Organisms/UnorderedListOfImagesWithText.js
@@ -1,14 +1,19 @@
-import { List } from '@material-ui/core';  
+import { List } from '@material-ui/core';
 import ListItemWithImageAndText from '../Molecules/ListItemWithImageAndText';
-import { lightTheme } from '../../theme';
 
 /**
  * Summary:
  * Displays a unordered list of images with text from an array that you can click on.
  * 
  * Props: 
- * listItemsArray: array of objects where each object contains the image, title, and action.
- * variant: a string that states which type of unordered list is rendered: sidebar or contacts
+ * listItemsArray: array of objects.
+ * 		[
+ * 			{	
+ * 				image: component such as circleImage atom,
+ * 				name: string to be displayed for the list item
+ * 				action: function to be ran onlClick of a list item.
+ * 			}
+ * 		]
  */
 export default props => {
 	let listItemsArray = [];
@@ -16,14 +21,14 @@ export default props => {
 	const renderListItemsToScreen = () => {
 		props.listItemsArray.forEach(item => {
 			listItemsArray.push(
-				<ListItemWithImageAndText text={item.name} key={keyForEachListItem}>
+				<ListItemWithImageAndText action={item.action} text={item.name} key={keyForEachListItem}>
 					{item.image}
 				</ListItemWithImageAndText>
 			);
 			keyForEachListItem++;
 		});
 	};
-	
+
 	return (
 		<div>
 			<List>
@@ -31,5 +36,5 @@ export default props => {
 				{listItemsArray}
 			</List>
 		</div>
-  	);
+	);
 }
