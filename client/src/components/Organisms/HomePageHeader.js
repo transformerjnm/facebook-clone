962f8932161a955b311c12ba52285aa900481d04
transformerjnm <46799492+transformerjnm@users.noreply.github.com>
2021-01-31 13:37:56 -0500

    Worked on the grid for the header #23
    Updated the grid for the header to be more responsive. At the breakpoints the content goes on two lines. If you mess with the button padding this goes away. Still needs some work but is getting closer.

--- client/src/components/Organisms/HomePageHeader.js
+++ client/src/components/Organisms/HomePageHeader.js
@@ -1,4 +1,4 @@
-import { Grid, IconButton, Hidden, Icon } from '@material-ui/core';  
+import { Grid, IconButton, Hidden, Icon } from '@material-ui/core';
 import NavigationIconLink from '../Atoms/NavigationIconLink';
 import CircleImageWithTextInput from '../Molecules/CircleImageWithTextInput';
 import HomeIcon from '@material-ui/icons/Home';
@@ -14,6 +14,8 @@ import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import { lightTheme } from '../../theme';
 import CircleImage from '../Atoms/CircleImage';
+import PersonIcon from '@material-ui/icons/Person';
+
 /**
  * Summary:
  * Responsive header for navigating the application. At the current stage in development we want this to be responsive and have dummy links.
@@ -22,12 +24,12 @@ import CircleImage from '../Atoms/CircleImage';
  * Props: 
  * userProfileImage: a string that is used to set a image's src attribute
  * imageAlt: a string for a image's alt attribute
- * imageDestination: a string thta sets the browser's url on click of the image
+ * imageDestination: a string that sets the browser's url on click of the image
  * authenticatedUsername: a string of the user that is currently logged in
  * showSearch: a boolean value to determine if the search pop up is showing
  * showIcons: a boolean value to determine if the icon pop up is showing
  * setShowSearch: a function that sets state to show the search pop up on small devices
- * setShowIcons: a fkunction that sets state to show the list of icons on small devices
+ * setShowIcons: a function that sets state to show the list of icons on small devices
  */
 export default props => {
 	const headerContainer = {
@@ -35,21 +37,24 @@ export default props => {
 		backgroundColor: lightTheme.palette.primary.light
 	};
 
-  	return (
-    	<div style={headerContainer}>
-     		<Grid container alignItems="center">
-				<Grid item lg={3} md={2} sm={2}>
+	return (
+		<div style={headerContainer}>
+			<Grid container alignItems="center">
+
+				<Grid item lg={3} md={2} xs={3}>
 					<Grid container alignItems="center">
+						{/**Desktop search bar */}
 						<Hidden mdDown>
 							<Grid item>
-									<CircleImageWithTextInput 
-										inputVariant="search"
-										imageSrc="https://cdn.iconscout.com/icon/free/png-256/facebook-224-498412.png"					
-										imageAlt="fb"
-										onImageClickDestination="/home"
-									/>
+								<CircleImageWithTextInput
+									inputVariant="search"
+									imageSrc="https://cdn.iconscout.com/icon/free/png-256/facebook-224-498412.png"
+									imageAlt="fb"
+									onImageClickDestination="/home"
+								/>
 							</Grid>
 						</Hidden>
+						{/**Mobile search bar */}
 						<Hidden lgUp>
 							<Grid item>
 								<CircleImage
@@ -61,17 +66,24 @@ export default props => {
 						</Hidden>
 						<Hidden lgUp>
 							<IconButton onClick={() => props.setShowSearch(!props.showSearch)}>
-								<SearchIcon/>
+								<SearchIcon />
 							</IconButton>
 						</Hidden>
 					</Grid>
 				</Grid>
+
+				{/** tablet and up navigation links */}
 				<Hidden smDown>
-					<Grid item lg={5} md={6} sm={8}>
+					<Grid item lg={5} md={6}>
 						<Grid container justify="center" alignItems="center">
 							<Grid item>
 								<NavigationIconLink navLinkDestination="/home">
 									<HomeIcon />
+								</NavigationIconLink>
+							</Grid>
+							<Grid item>
+								<NavigationIconLink navLinkDestination="/friends">
+									<PersonIcon />
 								</NavigationIconLink>
 							</Grid>
 							<Grid item>
@@ -84,65 +96,71 @@ export default props => {
 									<StorefrontIcon />
 								</NavigationIconLink>
 							</Grid>
-							<Grid item>
-								<NavigationIconLink navLinkDestination="/group">
-									<GroupIcon />
-								</NavigationIconLink>
-							</Grid>
+							<Hidden mdDown>
+								<Grid item>
+									<NavigationIconLink navLinkDestination="/group">
+										<GroupIcon />
+									</NavigationIconLink>
+								</Grid>
+							</Hidden>
 						</Grid>
 					</Grid>
 				</Hidden>
-				<Grid item lg={4} md={4} sm={6}>
-					<Grid container alignItems="center" justify="center" spacing={4}>
-						<Hidden lgUp>
-							<Grid item>
-								<IconButton onClick={() => props.setShowIcons(!props.showIcons)}>
-									<MenuIcon/>
-								</IconButton>
-							</Grid>
-						</Hidden>
-						<Hidden mdDown>
-							<Grid item>
-								<Grid container justify="flex-start">
-									<CircleImageWithTitle 
-										imageUrl={props.userProfileImage}
-										imageAlt={props.imageAlt}
-										imageClick={props.imageDestination}
-										title={props.authenticatedUsername}
-									/>
-								</Grid>
-							</Grid>
-						</Hidden>
-						<Grid item>
+
+				<Grid item lg={4} md={4} xs={9}>
+					<Grid container alignItems="center" justify="flex-end" spacing={4}>
+						<Grid item xs={10} lg={6}>
 							<Grid container justify="flex-end" spacing={1}>
+								{/**menu Icon to show on phones and tablets */}
+								<Hidden lgUp>
 									<Grid item>
-										<IconButton 
-											style={{backgroundColor: lightTheme.palette.primary.main }}
-											size="medium"
-										>
-											<AddIcon/>
+										<IconButton onClick={() => props.setShowIcons(!props.showIcons)}>
+											<MenuIcon />
 										</IconButton>
 									</Grid>
+								</Hidden>
+								{/** User profile link to show on laptops and up */}
+								<Hidden mdDown lg={6}>
 									<Grid item>
-										<IconButton style={{backgroundColor: lightTheme.palette.primary.main }}>
-											<MessageIcon/>
-										</IconButton>
+										<Grid container justify="flex-start">
+											<CircleImageWithTitle
+												imageUrl={props.userProfileImage}
+												imageAlt={props.imageAlt}
+												imageClick={props.imageDestination}
+												title={props.authenticatedUsername}
+											/>
+										</Grid>
 									</Grid>
-									<Grid item>
-										<IconButton style={{backgroundColor: lightTheme.palette.primary.main }}>
-											<NotificationsIcon/>
-										</IconButton>
-									</Grid>
-									<Grid item>
-										<IconButton style={{backgroundColor: lightTheme.palette.primary.main }}>
-											<ArrowDropDownIcon/>
-										</IconButton>
-									</Grid>
+								</Hidden>
+								<Grid item>
+									<IconButton
+										style={{ backgroundColor: lightTheme.palette.primary.main }}
+										size="medium"
+									>
+										<AddIcon />
+									</IconButton>
+								</Grid>
+								<Grid item>
+									<IconButton style={{ backgroundColor: lightTheme.palette.primary.main }}>
+										<MessageIcon />
+									</IconButton>
+								</Grid>
+								<Grid item>
+									<IconButton style={{ backgroundColor: lightTheme.palette.primary.main }}>
+										<NotificationsIcon />
+									</IconButton>
+								</Grid>
+								<Grid item>
+									<IconButton style={{ backgroundColor: lightTheme.palette.primary.main }}>
+										<ArrowDropDownIcon />
+									</IconButton>
+								</Grid>
 							</Grid>
 						</Grid>
 					</Grid>
 				</Grid>
-			 </Grid>
-    	</div>
-  	);
-}
+
+			</Grid>
+		</div>
+	);
+};
