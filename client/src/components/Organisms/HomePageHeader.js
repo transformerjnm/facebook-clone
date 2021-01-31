7726c8fd3053c187e8c9c891ec756dc2533a5d04
blaine young <blaine.young3553@gmail.com>
2021-01-31 11:52:04 -0500

    implemented material ui's Hidden component #23

--- client/src/components/Organisms/HomePageHeader.js
+++ client/src/components/Organisms/HomePageHeader.js
@@ -1,4 +1,4 @@
-import { Grid, IconButton, Hidden } from '@material-ui/core';  
+import { Grid, IconButton, Hidden, Icon } from '@material-ui/core';  
 import NavigationIconLink from '../Atoms/NavigationIconLink';
 import CircleImageWithTextInput from '../Molecules/CircleImageWithTextInput';
 import HomeIcon from '@material-ui/icons/Home';
@@ -10,6 +10,7 @@ import AddIcon from '@material-ui/icons/Add';
 import MessageIcon from '@material-ui/icons/Message';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import { lightTheme } from '../../theme';
 import CircleImage from '../Atoms/CircleImage';
@@ -19,7 +20,14 @@ import CircleImage from '../Atoms/CircleImage';
  * The mobile menu should work.
  * 
  * Props: 
- * 
+ * userProfileImage: a string that is used to set a image's src attribute
+ * imageAlt: a string for a image's alt attribute
+ * imageDestination: a string thta sets the browser's url on click of the image
+ * authenticatedUsername: a string of the user that is currently logged in
+ * showSearch: a boolean value to determine if the search pop up is showing
+ * showIcons: a boolean value to determine if the icon pop up is showing
+ * setShowSearch: a function that sets state to show the search pop up on small devices
+ * setShowIcons: a fkunction that sets state to show the list of icons on small devices
  */
 export default props => {
 	const headerContainer = {
@@ -30,63 +38,81 @@ export default props => {
   	return (
     	<div style={headerContainer}>
      		<Grid container alignItems="center">
-				<Grid item lg={3}>
+				<Grid item lg={3} md={2} sm={2}>
 					<Grid container alignItems="center">
-						<Grid item>
-							<Hidden mdDown>
-								<CircleImageWithTextInput 
-									inputVariant="search"
-									imageSrc="https://cdn.iconscout.com/icon/free/png-256/facebook-224-498412.png"					
-									imageAlt="fb"
-									onImageClickDestination="/home"
-								/>
-							</Hidden>
-							<Hidden lgUp>
+						<Hidden mdDown>
+							<Grid item>
+									<CircleImageWithTextInput 
+										inputVariant="search"
+										imageSrc="https://cdn.iconscout.com/icon/free/png-256/facebook-224-498412.png"					
+										imageAlt="fb"
+										onImageClickDestination="/home"
+									/>
+							</Grid>
+						</Hidden>
+						<Hidden lgUp>
+							<Grid item>
 								<CircleImage
 									src="https://cdn.iconscout.com/icon/free/png-256/facebook-224-498412.png"
 									alt="fb"
 									onImageClickDestination="/home"
 								/>
-							</Hidden>
-						</Grid>
+							</Grid>
+						</Hidden>
+						<Hidden lgUp>
+							<IconButton onClick={() => props.setShowSearch(!props.showSearch)}>
+								<SearchIcon/>
+							</IconButton>
+						</Hidden>
 					</Grid>
 				</Grid>
-				<Grid item lg={6}>
-					<Grid container justify="center" alignItems="center">
-						<Grid item>
-							<NavigationIconLink navLinkDestination="/home">
-								<HomeIcon />
-							</NavigationIconLink>
-						</Grid>
-						<Grid item>
-							<NavigationIconLink navLinkDestination="/live">
-								<LiveTvIcon />
-							</NavigationIconLink>
-						</Grid>
-						<Grid item>
-							<NavigationIconLink navLinkDestination="/marketplace">
-								<StorefrontIcon />
-							</NavigationIconLink>
-						</Grid>
-						<Grid item>
-							<NavigationIconLink navLinkDestination="/group">
-								<GroupIcon />
-							</NavigationIconLink>
-						</Grid>
-					</Grid>
-				</Grid>
-				<Grid item lg={3}>
-					<Grid container alignItems="center" justify="center" spacing={4}>
-						<Grid item>
-							<Grid container justify="flex-start">
-								<CircleImageWithTitle 
-									imageUrl="https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8bWFufGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=60"
-									imageAlt="authenticated user"
-									imageClick="/"
-									title="blaine"
-								/>
+				<Hidden smDown>
+					<Grid item lg={5} md={6} sm={8}>
+						<Grid container justify="center" alignItems="center">
+							<Grid item>
+								<NavigationIconLink navLinkDestination="/home">
+									<HomeIcon />
+								</NavigationIconLink>
+							</Grid>
+							<Grid item>
+								<NavigationIconLink navLinkDestination="/live">
+									<LiveTvIcon />
+								</NavigationIconLink>
+							</Grid>
+							<Grid item>
+								<NavigationIconLink navLinkDestination="/marketplace">
+									<StorefrontIcon />
+								</NavigationIconLink>
+							</Grid>
+							<Grid item>
+								<NavigationIconLink navLinkDestination="/group">
+									<GroupIcon />
+								</NavigationIconLink>
 							</Grid>
 						</Grid>
+					</Grid>
+				</Hidden>
+				<Grid item lg={4} md={4} sm={6}>
+					<Grid container alignItems="center" justify="center" spacing={4}>
+						<Hidden lgUp>
+							<Grid item>
+								<IconButton onClick={() => props.setShowIcons(!props.showIcons)}>
+									<MenuIcon/>
+								</IconButton>
+							</Grid>
+						</Hidden>
+						<Hidden mdDown>
+							<Grid item>
+								<Grid container justify="flex-start">
+									<CircleImageWithTitle 
+										imageUrl={props.userProfileImage}
+										imageAlt={props.imageAlt}
+										imageClick={props.imageDestination}
+										title={props.authenticatedUsername}
+									/>
+								</Grid>
+							</Grid>
+						</Hidden>
 						<Grid item>
 							<Grid container justify="flex-end" spacing={1}>
 									<Grid item>
