7b7023f2e7bd3fb197379f9539c1dbcb204e11f7
transformerjnm <46799492+transformerjnm@users.noreply.github.com>
2021-02-02 18:33:29 -0500

    Worked on the home page styles and user post styles. #25
    Added space aroud the post. Updated the home page grid to work better on extra large screens/ widescreen.

--- client/src/components/Pages/HomePage.js
+++ client/src/components/Pages/HomePage.js
@@ -19,63 +19,70 @@ import { lightTheme } from '../../theme';
  *
  */
 export default props => {
+	const scrollStyle = {
+		overflow: "scroll",
+		/** VH height minus header height */
+		height: "calc(100vh - 60px)",
+		scrollbarWidth: "none",
+		overflowStyle: "none",
+	}
 	return (
-		<>			
+		<>
 			<HomePageHeader
-                userProfileImage="https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8bWFufGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=60"
-                imagaeAlt="authenticated user"
-                imageDestination="/"
-                authenticatedUsername="blaine"
-            />				
-			<Grid container>
+				userProfileImage="https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8bWFufGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=60"
+				imagaeAlt="authenticated user"
+				imageDestination="/"
+				authenticatedUsername="blaine"
+			/>
+			<Grid container justify="center">
 				<Hidden mdDown>
-					<Grid item md={3}>
-						<UnorderedListOfImagesWithText 
-							listItemsArray={ [
-									{
-										image: <HomeIcon fontSize="large"/>,
-										name: 'Home',
-										action: null
-									},
-									{
-										image: <PeopleIcon fontSize="large" />,
-										name: 'Friends',
-										action: null
-									},
-									{
-										image: <StorefrontIcon fontSize="large" />,
-										name: 'Marketplace',
-										action: null
-									},
-									{
-										image: <BookmarkIcon fontSize="large" />,
-										name: 'Save',
-										action: null
-									},
-									{
-										image: <CircleImage 
-											alt="girl" 
-											onImageClickDestination="/"
-											src="https://images.unsplash.com/photo-1563306406-e66174fa3787?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Nnx8Z2lybHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60" 
-										/>,
-										name: 'Mary', 
-										action: null,										
-									}
-								] 
-							} 
+					<Grid item md={3} lg={3} xl={2} style={scrollStyle}>
+						<UnorderedListOfImagesWithText
+							listItemsArray={[
+								{
+									image: <HomeIcon fontSize="large" />,
+									name: 'Home',
+									action: null
+								},
+								{
+									image: <PeopleIcon fontSize="large" />,
+									name: 'Friends',
+									action: null
+								},
+								{
+									image: <StorefrontIcon fontSize="large" />,
+									name: 'Marketplace',
+									action: null
+								},
+								{
+									image: <BookmarkIcon fontSize="large" />,
+									name: 'Save',
+									action: null
+								},
+								{
+									image: <CircleImage
+										alt="girl"
+										onImageClickDestination="/"
+										src="https://images.unsplash.com/photo-1563306406-e66174fa3787?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Nnx8Z2lybHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60"
+									/>,
+									name: 'Mary',
+									action: null,
+								}
+							]
+							}
 						/>
 					</Grid>
 				</Hidden>
-				<Grid item md={9} lg={6}>
+				<Grid item md={9} lg={6} style={scrollStyle}>
 					<Container>
-					<CreateNewPost
-						inputVariant="newPost"
-						imageSrc="https://images.unsplash.com/photo-1611615748492-99a6ad27548a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
-						imageAlt="francesca Zama selfie"
-						onInputSubmit={() => { }}
-						onImageClickDestination="/"
-					/>
-					<UserPost
+						<CreateNewPost
+							inputVariant="newPost"
+							imageSrc="https://images.unsplash.com/photo-1611615748492-99a6ad27548a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
+							imageAlt="francesca Zama selfie"
+							onInputSubmit={() => { }}
+							onImageClickDestination="/"
+						/>
+						<UserPost
 							postText="The National Mall in Washington, D.C., was decorated on Monday with close to 200,000 flags representing the people who are unable to attend President-elect Joe Biden’s inauguration because of the coronavirus pandemic and restrictive security measures"
 							authorImageUrl="https://images.unsplash.com/photo-1610984474942-201775d5ee9d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
 							authorImageAlt="Author image"
@@ -147,63 +154,63 @@ export default props => {
 					</Container>
 				</Grid>
 				<Hidden smDown>
-					<Grid item lg={3}>
+					<Grid item md={3} lg={3} xl={2} style={scrollStyle}>
 						{/** TODO: Typographyt should be made into its own componetn with search icon */}
 						<Typography>Contacts</Typography>
-						<UnorderedListOfImagesWithText 
-							listItemsArray={ 
+						<UnorderedListOfImagesWithText
+							listItemsArray={
 								[
 									{
-									image: <CircleImage 
-										alt="girl" 
-										onImageClickDestination="/"
-										isOnline={false}
-										src="https://images.unsplash.com/photo-1563306406-e66174fa3787?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Nnx8Z2lybHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60" 
-									/>,
-									name: 'Blaine', 
-									action: null,										
+										image: <CircleImage
+											alt="girl"
+											onImageClickDestination="/"
+											isOnline={false}
+											src="https://images.unsplash.com/photo-1563306406-e66174fa3787?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Nnx8Z2lybHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60"
+										/>,
+										name: 'Blaine',
+										action: null,
 									},
 									{
-									image: <CircleImage 
-										alt="girl" 
-										onImageClickDestination="/"
-										isOnline={true}
-										src="https://images.unsplash.com/photo-1563306406-e66174fa3787?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Nnx8Z2lybHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60" 
-									/>,
-									name: 'Jacob', 
-									action: null,										
+										image: <CircleImage
+											alt="girl"
+											onImageClickDestination="/"
+											isOnline={true}
+											src="https://images.unsplash.com/photo-1563306406-e66174fa3787?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Nnx8Z2lybHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60"
+										/>,
+										name: 'Jacob',
+										action: null,
 									},
 									{
-									image: <CircleImage 
-										alt="girl" 
-										onImageClickDestination="/"
-										isOnline={true}
-										src="https://images.unsplash.com/photo-1563306406-e66174fa3787?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Nnx8Z2lybHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60" 
-									/>,
-									name: 'Jill', 
-									action: null,										
+										image: <CircleImage
+											alt="girl"
+											onImageClickDestination="/"
+											isOnline={true}
+											src="https://images.unsplash.com/photo-1563306406-e66174fa3787?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Nnx8Z2lybHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60"
+										/>,
+										name: 'Jill',
+										action: null,
 									},
 									{
-									image: <CircleImage 
-										alt="girl" 
-										onImageClickDestination="/"
-										isOnline={true}
-										src="https://images.unsplash.com/photo-1563306406-e66174fa3787?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Nnx8Z2lybHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60" 
-									/>,
-									name: 'Joe', 
-									action: null,										
+										image: <CircleImage
+											alt="girl"
+											onImageClickDestination="/"
+											isOnline={true}
+											src="https://images.unsplash.com/photo-1563306406-e66174fa3787?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Nnx8Z2lybHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60"
+										/>,
+										name: 'Joe',
+										action: null,
 									},
 									{
-									image: <CircleImage 
-										alt="girl" 
-										onImageClickDestination="/"
-										src="https://images.unsplash.com/photo-1563306406-e66174fa3787?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Nnx8Z2lybHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60" 
-									/>,
-									name: 'Mary', 
-									action: null,										
+										image: <CircleImage
+											alt="girl"
+											onImageClickDestination="/"
+											src="https://images.unsplash.com/photo-1563306406-e66174fa3787?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Nnx8Z2lybHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60"
+										/>,
+										name: 'Mary',
+										action: null,
 									}
-								] 
-							} 
+								]
+							}
 						/>
 					</Grid>
 				</Hidden>
